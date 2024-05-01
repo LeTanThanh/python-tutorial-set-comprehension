4524d72730eb7255a942b9cba06e53fd13f5a957
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 15:36:39 +0700

    Introduction to Python Set comprehension

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 if __name__ == "__main__":
-  print("Python Set Comprehension")
+  # Introduction to Python Set comprehension
+
+  tags = {  "Django", "Pandas", "Numpy" }
+  print(tags)
+
+  lower_tags = set()
+  for tag in tags:
+    lower_tags.add(tag.lower())
+  print(lower_tags)
+
+  lower_tags = set(map(lambda tag: tag.lower() , tags))
+  print(lower_tags)
+
+  lower_tags = { tag.lower() for tag in tags }
+  print(lower_tags)
+
+  new_tags = { tag.lower() for tag in tags if tag != "Numpy" }
+  print(new_tags)
